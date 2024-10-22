d034731ef1224730b767c5cc2515cbbcbd262331
Christelle Riamaidje Bienvenu <tasiochristelle@yahoo.com>
2024-10-22 23:42:35 +0200

    init

---
+++ src/main/java/de/htwberlin/webtech/NotizblockApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotizblockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotizblockApplication.class, args);
+	}
+
+}
